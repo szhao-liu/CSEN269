0ecf04ce9086362320773bb95581cccaef5d87de
Anwitha Arbi <38724750+Anwitha2806@users.noreply.github.com>
2024-11-26 09:42:39 -0800

    Updated gethelp button

--- lib/features/user_auth/presentation/pages/Tasks.dart
+++ lib/features/user_auth/presentation/pages/Tasks.dart
@@ -354,6 +354,31 @@ class _TasksPageState extends State<TasksPage> {
                 ],
               ),
             ),
+            Positioned(
+              bottom: 20,
+              right: 20, // Positioned to the bottom right
+              child: GestureDetector(
+                onTap: () {
+                  //Navigate to GetHelpPage when the button is pressed
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GetHelpPage()),
+                  );
+                },
+                child: CircleAvatar(
+                  radius: 25, // Smaller size for the button
+                  backgroundColor: Colors.blueAccent,
+                  child: Text(
+                    "?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25, // Adjusted font size for the "?" text
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
